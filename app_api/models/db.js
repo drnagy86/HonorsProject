b471bc87fcc1bbe2a16fcc41b6a321e2abdb56cd
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2022-05-13 09:28:40 -0500

    Add production db

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,25 +1,27 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // original
-let url = 'mongodb://localhost/rubricDB';
-
-
-
+// let url = 'mongodb://localhost/rubricDB';
+// let url = process.env.DB_URI;
 
 // doesn't crash, can access express, but still no db
 //let url = `mongodb://root:123456@$localhost:27017/rubricDB?authSource=admin`;
 
 
 
+const username = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const host = process.env.MONGODB_HOST;
+let url =
+    `mongodb+srv://${username}:${password}@${host}/${cluster}?retryWrites=true&w=majority`;
 
 
 require ('./rubrics');
 require ('./users');
 
-// to remind about setting up for production
-// if (process.env.NODE_ENV === 'production') {
-//     dbURI = 'mongodb://mongo:27017/rubricDB';
-// }
+
 const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -34,36 +36,38 @@ const options = {
     useUnifiedTopology: true
 }
 
+
+// works
 mongoose.connect(url, {
     useNewUrlParser: true
 });
-// mongoose.connection.on('connected', () => {
-//     console.log(`Mongoose connected to ${url}`);
-// });
-// mongoose.connection.on('error', err => {
-//     console.log(`Mongoose connection error: ${err}`);
-// });
-// mongoose.connection.on('disconnected', () => {
-//     console.log('Mongoose disconnected');
-// });
-// const gracefulShutdown = (msg, callback) => {
-//     mongoose.connection.close( () => {
-//         console.log(`Mongoose disconnected through ${msg}`);
-//         callback();
-//     });
-// };
-// // For nodemon restarts
-// process.once('SIGUSR2', () => {
-//     gracefulShutdown('nodemon restart', () => {
-//         process.kill(process.pid, 'SIGUSR2');
-//     });
-// });
-// // For app termination
-// process.on('SIGINT', () => {
-//     gracefulShutdown('app termination', () => {
-//         process.exit(0);
-//     });
-// });
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${url}`);
+});
+mongoose.connection.on('error', err => {
+    console.log(`Mongoose connection error: ${err}`);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
 
 require('./rubrics');
 
